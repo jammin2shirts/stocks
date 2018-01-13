fd2b4e6e7493b03ca09308ced9d9c569e6c4ac37
jammin2shirts <teuchertb@gmail.com>
2018-01-13 00:21:21 -0500

    connected to database successfully

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 const express = require("express");
 const app = express();
+const { Client } = require('pg');
+const connectionString = process.env.DATABASE_URL || 'postgres://ben:Twisted1@localhost:5432/local';
+const client = new Client ({
+  connectionString: connectionString 
+});
+
+
+client.connect((err) => {
+  if (err) {
+    console.error('connection error', err.stack)
+  } else {
+    console.log('connected')
+  }
+});
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
