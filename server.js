fa822da1aa41b019d3f26884a1def71ae95d2164
jammin2shirts <teuchertb@gmail.com>
2018-01-13 12:40:30 -0500

    added random step

--- server.js
+++ server.js
@@ -15,6 +15,28 @@ client.connect((err) => {
   }
 });
 
+client.query('SELECT * from tmp')
+.then(res => console.log(res.rows))
+.catch(e => console.error(e.stack));
+
+var insertValue = [5];
+var insertQuery = function (err, res) {
+  client.query('INSERT INTO tmp (id) values ($1) returning *', insertValue)
+  .then(res => console.log(res.rows[0]))
+  .catch(e => console.error(e.stack))
+};
+
+var updateSetValue = [9];
+var updateQualifierValue = [5];
+var updateQuery = function (err, res) {
+  client.query('UPDATE tmp SET id = $1 WHERE id = $2', updateSetValue, updateQualifierValue)
+  .then(res => console.log('successful update'))
+  .catch(e => console.error(e.stack))
+};
+
+//insertQuery();
+updateQuery();
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(3000, ()=> console.log('Server running on port 3000'));
